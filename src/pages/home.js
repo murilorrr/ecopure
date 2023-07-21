d3c9b7425ab346f11edcd0cc729fa0bd7332d0e1
murilorsv14 <murilorsv14@gmail.com>
2023-07-20 22:49:41 -0300

    feat(home page): inicio da paginacao de home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import HeroBanner from "../app/components/HeroBanner";
+import Carrousel from "../app/components/Carrousel";
+import Depoimentos from "../app/components/Depoimentos";
+import Contatos from "../app/components/Contatos";
+import DesenvolvidoPor from "../app/components/DesenvolvidoPor";
+import Header from "../app/components/Header";
+import Footer from "../app/components/Footer";
 
-const AboutPage = () => {
+const HomePage = () => {
   return (
     <div>
-      <p>About</p>
+      <Header></Header>
+      <HeroBanner></HeroBanner>
+      <Carrousel></Carrousel>
+      <Depoimentos></Depoimentos>
+      <Contatos></Contatos>
+      <Footer></Footer>
     </div>
   );
 };
 
-export default AboutPage;
+export default HomePage;
